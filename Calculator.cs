1d1d4e3648887d83d3b4471edc795ec0ecd006ae
Knarik-Dabaghyan <69726910+Knarik-Dabaghyan@users.noreply.github.com>
2020-08-18 16:44:47 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace ConsoleApplication64
+{
+    public class Calculator
+    {
+        private string expression { set; get; }
+        private double res { set; get; }
+
+
+        public Calculator()
+        {
+            Console.WriteLine("Please enter an expression like this -> 1 + 1");
+            expression = Console.ReadLine();
+        }
+        public double Result()
+        {
+
+            int tmp = expression.IndexOfAny(new char[] { '+', '-', '/', '*', '%' }), i = 0;
+            var y = (expression.Split(new char[] { '+', '-', '/', '*', '%' }));
+            switch (expression[tmp])
+            {
+                case '+':
+                    res = int.Parse(y[i]) + int.Parse(y[++i]);
+                    break;
+                case '-':
+                    res = int.Parse(y[i]) - int.Parse(y[++i]);
+                    break;
+                case '*':
+                    res = int.Parse(y[i]) * int.Parse(y[++i]);
+                    break;
+                case '/':
+                    res = int.Parse(y[i]) / int.Parse(y[++i]);
+                    break;
+                case '%':
+                    res = int.Parse(y[i]) % int.Parse(y[++i]);
+                    break;
+                default:
+                    break;
+
+            }
+            return res;
+        }
+
+            public void Show( ){
+
+            WriteLine($"Result={Result()}");
+        }
+
+        
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace ConsoleApplication64
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Calculator calculator = new Calculator();
+                calculator.Show();
+            
+        
+
+
+
+
+
+          
+          
+
+            }
+        }
+    }
